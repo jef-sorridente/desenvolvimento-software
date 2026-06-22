32b3ec7f1cdd7eb958f6c95f019d37555a5043da
jef-sorridente <jpadilha53@gmail.com>
2026-06-22 10:51:42 -0300

    chore: remove arquivo de teste manual

--- test-manual.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const { sendPromptToGemini } = require("./app");
-
-const apiKey = process.env.GEMINI_API_KEY;
-
-sendPromptToGemini("Qual é a capital do Brasil?", apiKey)
-  .then((resposta) => console.log("Resposta:", resposta))
-  .catch((err) => console.error("Erro:", err.message));
